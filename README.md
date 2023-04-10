692f4e460c4eeceba30f1e3a1da5244855ca4413
TwoOnefour <lys214412@gmail.com>
2023-04-10 19:39:27 +0800

    update

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 湖北地区的青年大学习自动打卡，本程序适合ios+qq自带的qq邮箱提醒直接开盖使用
 # Usage
 ## Modify crontab configuration
-整个文件夹下载到linux上，下载geckodriver到/usr/local/bin文件夹下，使用crontab，配置如下
+~~整个文件夹下载到linux上，下载geckodriver到/usr/local/bin文件夹下~~，已经更新，没有必要使用geckodriver
 
-<code>57 23 * * * rm -rf /home/admin/myYouthStudy/tmp/*</code>
+使用crontab，配置如下
 
-<code>0 12 * * 1 python3 /home/admin/myYouthStudy/youthSpider.py > /root/youthSpider.log 2>&1 </code>
+已经更新，没有必要使用geckodriver
+
+<code>0 12 * * 1 python3 /your/script/path/youthSpider.py > /root/youthSpider.log 2>&1 </code>
 
 :wq保存
 
@@ -26,9 +28,8 @@
 ![image](https://user-images.githubusercontent.com/77989499/224948956-11a88f02-233c-436e-9345-9eca59499a4f.png)
 ### Submit information
 
-![image](https://user-images.githubusercontent.com/77989499/224949294-521fcece-04bb-4197-a814-fe2596a3b4c6.png)
 
-建议自行抓包填写
+建议自行抓包填写,只用填写openid即可，位于youthSpider.py的12行
 
 ### Not a user of IOS?
 如果不是ios用户，可以自行修改截图图片部分
@@ -37,7 +38,7 @@
 
 # (NOTE)IT MAY NOT WORK
 
-由于没有鉴别手段，无法验证接口正确性，建议只当作一个自动截图使用，不保证后台有观看数据。
+由于没有鉴别手段，无法验证接口正确性，建议只当作一个自动截图使用，不保证后台有观看数据，但大概率接口是对的（自信）。
 ![558E4669DE897CF0A70183480A5FFC8A](https://user-images.githubusercontent.com/77989499/224951932-412fd8f3-b170-48e7-a46e-51b86cd4b449.png)
 
 ![UEPQ5E3`7ASFN X`P1A3F6F](https://user-images.githubusercontent.com/77989499/224951217-7bbb7458-9fd3-48d9-92d6-a478c2b6332e.png)
--- youthSpider.py
+++ youthSpider.py
@@ -1,53 +1,61 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-import cv2
-import numpy as np
-from pyvirtualdisplay import Display
-import time
-import sys
-import urllib.parse
-import execjs
-import requests
 import json
-from lxml import etree
-import cn2an
 import os
+import random
 import smtplib
-import urllib3
+import time
+import urllib.parse
+from email import encoders
+from email.header import Header
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
-from email.header import Header
-from email import encoders
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+
+import cv2
+import numpy as np
+import requests
+import urllib3
+from lxml import etree
+
 urllib3.disable_warnings()
-class fkYouthStudy():
+
+
+class FkYouthStudy:
     def run(self):
-        name = '' # 你自己的名字
-
-
-        self.get_jpg()
-
+        self.openid = ""  # 只需填入自己的openid
+        self.get_jpg(self.get_str())
+        self.submit_data(self.get_data())
         self.mail()
 
-    def submit_data(self,name, n):
+    def get_data(self):
+        return requests.get("https://api.fjg360.cn/index.php", params={
+            'm': 'vote',
+            'c': 'index',
+            'a': 'get_members',
+            'openid': self.openid,
+            "callback": "jQuery190{}_{}".format(str(random.random()).replace(".", ""), int(time.time() * 1000)),
+            "_": int(time.time_ns())
+        },verify=False)
+
+    def submit_data(self, n):
+        n = json.loads(n.text.split("(")[1].strip(")"))
         data = { # 这里建议自己抓包，接口是https://cp.fjg360.cn/index.php
             'm': 'vote',
             'c': 'index',
             'a': 'save_door',
-            'username': name,
-            "openid": "", # 你的openid
-            'city': '', # 大学名字
-            'danwei2': '', # xx班团支部
-            'danwei': '', # xx学院
-            'lesson_name': '"青年大学习"2023年第' + n + '期',
+            'username': n["h5_ask_member"]["name"],
+            "openid": self.openid, # 你的openid
+            'city': n["h5_ask_member"]["danwei1"], # 大学名字
+            'danwei2': n["h5_ask_member"]["danwei2"], # xx班团支部
+            'danwei': n["h5_ask_member"]["danwei3"], # xx学院
+            'lesson_name': urllib.parse.quote(self.title),
             '_':int(time.time() * 1000),
             'sessionId':'',
             'imgTextId':'',
             'ip':'',
             'phone':'未知',
             'num':10,
+            "callback": "jQuery190{}_{}".format(random.random(), int(time.time() * 1000))
         }
         headers = {
         "accept": "text / javascript,application / javascript, application / ecmascript, application / x - ecmascript, * / *; q = 0.01",
@@ -58,80 +66,46 @@ class fkYouthStudy():
         "sec - fetch - dest": "empty",
         "accept - encoding": "gzip, deflate, br",
         "accept - language": "zh - CN, zh;q = 0.9, en - US;q = 0.8, en;q = 0.7"
-
         }
-        response = requests.get('https://cp.fjg360.cn/index.php', params=data, verify=False,headers=headers)
+        requests.get('https://cp.fjg360.cn/index.php', params=data, verify=False,headers=headers)
 
-
-
-    def get_jpg(self): # 切割图片
-        h = self.get_str()
-        self.jstopy()
+    def get_jpg(self, h):  # 切割图片
+        if os.path.exists("./tmp"):
+            for i in os.listdir(os.getcwd() + "/tmp"):
+                os.remove(os.getcwd() + "/tmp/" + i)
+        else:
+            os.mkdir("./tmp")
         response = requests.get('https://h5.cyol.com/special/daxuexi/' + h + '/images/end.jpg', verify=False).content
-        f = open("{}.jpg".format("/home/admin/myYouthStudy/tmp/tmp1"), mode="wb")
+
+        f = open("{}.jpg".format("./tmp/tmp1"), mode="wb")
         f.write(response)
         f.close()
-        image = cv2.imread("/home/admin/myYouthStudy/tmp/tmp1.jpg")
-        imagetemp1 =  cv2.imread("/home/admin/myYouthStudy/1.jpg")
+        image = cv2.imread("tmp/tmp1.jpg")
+        imagetemp1 = cv2.imread("1.jpg")
         image1 = cv2.resize(image,(1170, 2008),interpolation=cv2.INTER_AREA)
-        imagetemp = cv2.imread("/home/admin/myYouthStudy/temp.jpg")
+        imagetemp = cv2.imread("temp.jpg")
         image2 = cv2.resize(imagetemp,(1170,275),interpolation=cv2.INTER_AREA)
         imagetemp1 = cv2.resize(imagetemp1,(1170,249),interpolation=cv2.INTER_AREA)
         image3 = np.vstack([image2,image1,imagetemp1])
-
-        cv2.imwrite("/home/admin/myYouthStudy/tmp/{}.jpg".format(self.title), image3)
-    
+        cv2.imencode('.jpg', image3)[1].tofile(r'./tmp/{}.jpg'.format(self.title))
 
     def get_str(self):
-        # headers = {
-        #     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        #     'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
-        #     'Connection': 'keep-alive',
-        #     'Upgrade-Insecure-Requests': '1',
-        #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0",
-        #     "Accept-Encoding": "gzip, deflate",
-        #     'Host':'news.cyol.com'
-        # }
-        #
-        # response = requests.get('http://news.cyol.com/gb/channels/vrGlAKDl/index.html', headers=headers, verify=False)
-        # f = open("mainhtml.html", mode="wb")
-        # f.write(response.content)
-        # # local_file_path = './大.html'
-        # parser = etree.HTMLParser(encoding="utf-8")
-        # # tree = etree.parse(local_file_path)
-        # tree = etree.HTML(response.text)
-        # # result = tree.xpath('//ul[@class="movie-list"]/text()')
-        # try:
-        #     result = dict(tree.xpath("/html/body/div[2]/div/ul/li[1]/h3/a")[0].attrib)["href"]
-        #     self.url = result
-        # except IndexError as e:
-        #     print("爬虫限制")
-        #     self.close()
-        # url_str = result.split('/')
-        # f.close()
-        # return url_str[5]
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0",
             "Host": "h5.cyol.com"
         }
         response = requests.get('http://h5.cyol.com/special/weixin/sign.json', headers=headers, verify=False)
         dict1 = json.loads(response.text)
-        count = 0
-        for i in dict1:
-            if count == len(dict1) - 1:
-                self.url = dict1[i]["url"]
-            count += 1
+        for i in dict.__reversed__(dict1):
+            self.url = dict1[i]["url"]
+            break
         url_str = dict1[i]["url"].split('/')
         response = requests.get(self.url,headers={
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0"
         },verify=False)
-        response = requests.get(self.url,headers={
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0"
-        },verify=False)
-        parser = etree.HTMLParser(encoding="utf-8")
+        etree.HTMLParser(encoding="utf-8")
         # tree = etree.parse(local_file_path)
         tree = etree.HTML(response._content.decode("utf-8"))
-
         self.title = tree.xpath("/html/head/title")[0].text
         return url_str[5]
 
@@ -143,8 +117,8 @@ class fkYouthStudy():
             my_user = ''  # 收件人邮箱账号，我这边发送给自己
             message = MIMEMultipart()
             m_img = MIMEBase('', '')
-            m_img.add_header('Content-Disposition', 'attachment', filename="{}   .jpg".format(self.title))
-            fd = open("{}.jpg".format("/home/admin/myYouthStudy/tmp/"+self.title), "rb")  # 读取本地图片
+            m_img.add_header('Content-Disposition', 'attachment', filename="{}".format(self.title))
+            fd = open("{}.jpg".format("./tmp/"+self.title), "rb")  # 读取本地图片
             m_img.set_payload(fd.read())
             encoders.encode_base64(m_img)
             message.attach(m_img)
@@ -160,62 +134,8 @@ class fkYouthStudy():
             ret = False
             print(e)
         return ret
-    def close(self):
-        sys.exit()
-    def jstopy(self):
-        display = Display(visible=0, size=(900, 800))
-        display.start()  # 显示界面的设置
-        browser = webdriver.Firefox(executable_path="{}/geckodriver".format("/usr/local/bin"))
-        lesson_name = urllib.parse.quote(self.title)
-        # https://cp.fjg360.cn/index.php?m=vote&c=index&a=save_door&username=%E8%93%9D%E6%A5%A0%E6%A5%A0&openid=omLPUjiM4XOmfhlBKLawP2p9fWZ0&city=%E6%AD%A6%E6%B1%89%E7%90%86%E5%B7%A5%E5%A4%A7%E5%AD%A6&danwei2=%E7%89%A9%E7%AE%A12001%E5%9B%A2%E6%94%AF%E9%83%A8&danwei=%E4%BA%A4%E9%80%9A%E4%B8%8E%E7%89%A9%E6%B5%81%E5%B7%A5%E7%A8%8B%E5%AD%A6%E9%99%A2&lesson_name="青年大学习"2022年第28期&_=${Date.now()}&sessionId=&imgTextId=&ip=&phone=%E6%9C%AA%E7%9F%A5&num=10
-        with open("{}/index.html".format("/home/admin/myYouthStudy/tmp"),"w") as f:
-            f.write('''<!DOCTYPE html>
-<html lang="zh">
-<head>
-    <meta charset="UTF-8">
-    <title>mytestpage</title>
 
-<style type = "text/css">
 
-</style>
-<script type="text/javascript" src="https://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
-<script type="text/javascript" src="https://lib.baomitu.com/jquery/1.9.1/jquery.min.js"></script>
-<script type="text/javascript" src="http://cdnsite.fjg360.cn/jquery-1.9.1.min.js"></script>
-<script charset="">
-    function ajaxJsonp(){
-	$.ajax({
-				type: 'GET',
-				url: `https://cp.fjg360.cn/index.php?m=vote&c=index&a=save_door&username=%E8%93%9D%E6%A5%A0%E6%A5%A0&openid=omLPUjiM4XOmfhlBKLawP2p9fWZ0&city=%E6%AD%A6%E6%B1%89%E7%90%86%E5%B7%A5%E5%A4%A7%E5%AD%A6&danwei2=%E7%89%A9%E7%AE%A12001%E5%9B%A2%E6%94%AF%E9%83%A8&danwei=%E4%BA%A4%E9%80%9A%E4%B8%8E%E7%89%A9%E6%B5%81%E5%B7%A5%E7%A8%8B%E5%AD%A6%E9%99%A2&lesson_name='''+lesson_name+'''&_=${Date.now()}&sessionId=&imgTextId=&ip=&phone=%E6%9C%AA%E7%9F%A5&num=10`,
-	        	dataType: 'jsonp',  //类型
-	        	data:  null ,
-				jsonp: 'callback', //jsonp回调参数，必需
-				async: false,
-				// error: errorHandle,
-	        	// success: sucessHandle
-	});
-}
-</script>
-</head>
-<body>
-
-<button onclick="ajaxJsonp()">123123</button>
-  <iframe frameborder="0" scrolling="no" style="width: 100%;height:1080px;"></iframe>
-        <center style="padding: 0;margin:0;list-style: none">
-            <br><a href="https://beian.miit.gov.cn">桂ICP备2022003330号</a>
-        </center>
-
-</body>
-</html>''')
-        # display = Display(visible=0, size=(900, 800))
-        # display.start()  # 显示界面的设置
-
-        wait = WebDriverWait(webdriver,10,0.5)
-        browser.get("file://{}/index.html".format("/home/admin/myYouthStudy/tmp"))
-        wait.until(lambda diver:browser.find_element(By.XPATH,"/html/body/button"))
-        browser.find_element(By.XPATH,"/html/body/button").click()
-        browser.close()
-        display.stop()
 if __name__ == "__main__":
-    spider = fkYouthStudy()
+    spider = FkYouthStudy()
     spider.run()
-    # spider.jstopy()
